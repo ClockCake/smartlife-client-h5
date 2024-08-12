62028147eac8fd6f37833882c4d1f3479a3935fc
ClockCat <hyd1234@outlook.com>
2024-08-12 15:58:51 +0800

    chore: setting devServer

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080, // 你可以改变这个端口号
+    allowedHosts: 'all',
+    client: {
+      webSocketURL: {
+        hostname: '0.0.0.0',
+        pathname: '/ws',
+        port: 8080, // 确保这与上面的端口匹配
+      },
+    },
+  }
 })
