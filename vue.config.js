8ecb40f4f30bad79cd12528527dfd7de204d21ef
ClockCat <hyd1234@outlook.com>
2024-08-12 17:32:14 +0800

    fix:bug

--- vue.config.js
+++ vue.config.js
@@ -6,11 +6,7 @@ module.exports = defineConfig({
     host: '0.0.0.0',
     port: 8080, // 您可以根据需要更改这个端口
     allowedHosts: 'all',
-    client: {
-      webSocketURL: 'ws://0.0.0.0:8080/ws',
-    },
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    }
+    webSocketServer: false // 这里禁用 WebSocket
+
   },
 })
