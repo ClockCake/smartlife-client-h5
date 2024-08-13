f2502ea40fb998e42254c96a615036aedb9a6d22
yao yao <hyd1234@outlook.com>
2024-08-13 23:17:35 +0800

    fix:bug

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,6 @@ module.exports = defineConfig({
     host: '0.0.0.0',
     port: 8080, // 您可以根据需要更改这个端口
     allowedHosts: 'all',
-    // webSocketServer: false,// 这里禁用 WebSocket
-
+    webSocketServer: false // 这里禁用 WebSocket
   },
 })
